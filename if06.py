47f78b4922305bf4cc2625ac24c23034a5253823
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2023-12-22 20:40:32 +0500

    6-vazifa tugadi

--- if06.py
+++ if06.py
@@ -6,4 +6,25 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1=n%10
+    n//=10
+    n2=n%10
+    n//=10
+    n3=n%10
+    n//=10
+    n4=n%10
+    n//=10
+    n5=n%10
+    max=n1
+    s=1
+    if n2>max:
+        max=n2
+        s=2
+    if n3>max:
+        max=n3
+        s=3
+    if n5>max:
+        max=n5
+        s=5
+    return s
+    
